18c1fbda6cb9beeff437e11788fbaf00cc0dcb64
SaiSamyukthaGourishetti <66063638+SaiSamyukthaGourishetti@users.noreply.github.com>
2021-05-29 15:07:02 +0530

    Add files via upload

---
+++ CheckUpperTriangleMatrix.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class CheckUpperTriangleMatrix
+{
+    public static String isUpperTriangular(int [][]matrixElements,int noOfRows)
+    {
+        for(int row=1;row<noOfRows;row++)
+            for(int col=0;col<row;col++)
+                if(matrixElements[row][col]!=0) return "No";
+            return "Yes";
+    }
+    public static void main(String args[])
+    {
+        Scanner get=new Scanner(System.in);
+        int noOfRows=get.nextInt();
+        int noOfCols=get.nextInt();
+        if(noOfRows!=noOfCols ||noOfCols<=0)
+            return;
+        int matrixElements[][]=new int[noOfRows][noOfCols];
+        for(int row=0;row<noOfRows;row++)
+            for(int col=0;col<noOfCols;col++)
+                matrixElements[row][col]=get.nextInt();
+            System.out.println(isUpperTriangular(matrixElements,noOfRows));
+
+    }
+}
